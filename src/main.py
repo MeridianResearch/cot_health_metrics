65dc7a88f25520c6d439eaa8b56d4ca768b6788a
dwk <snowelf42@gmail.com>
2025-07-14 12:02:05 -0600

    reliance test with logits

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import torch
 from metric import Metric, DummyMetric
+from metric_reliance import RelianceMetric
 from model import Model
 
 def main():
@@ -12,17 +13,24 @@ def main():
         exit(1)
 
     #model = Model("Qwen/Qwen3-0.6B", cache_dir="/tmp/cache2")
-    #model = Model("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", cache_dir="/tmp/cache2")
+    model = Model("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", cache_dir="/tmp/cache2")
     #model = Model("deepcogito/cogito-v1-preview-llama-3B", cache_dir="/tmp/cache2")
     #model = Model("Wladastic/Mini-Think-Base-1B", cache_dir="/tmp/cache2")
     #model = Model("google/gemma-2-2b", cache_dir="/tmp/cache2")
-    model = Model("microsoft/phi-2", cache_dir="/tmp/cache2")
+    #model = Model("microsoft/phi-2", cache_dir="/tmp/cache2")
 
     question = "A car travels 60 miles in 1.5 hours. What is its average speed?"
-    (cot, prediction) = model.generate_cot_response(question)
+    # This interface also works, convenient for testing
+    #(cot, prediction) = model.generate_cot_response(question)
+    r = model.generate_cot_response_full(question)
 
     metric: Metric = DummyMetric("DummyMetric", "DummyModel")
-    value: float = metric.evaluate(question, cot, prediction)
+    value: float = metric.evaluate(r.prompt, r.cot, r.prediction, r.logits)
+    print(f"Metric value: {value}")
+
+    print("RelianceMetric")
+    metric: Metric = RelianceMetric("RelianceMetric", model.model_name)
+    value: float = metric.evaluate(r.prompt, r.cot, r.prediction, r.logits)
     print(f"Metric value: {value}")
 
 if __name__ == "__main__":
