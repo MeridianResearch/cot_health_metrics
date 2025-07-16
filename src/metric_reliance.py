ee3204b02e1e360f459cd10a924ebdc8430dc600
dwk <snowelf42@gmail.com>
2025-07-16 16:06:46 -0600

    working Reliance

--- src/metric_reliance.py
+++ src/metric_reliance.py
@@ -6,13 +6,12 @@ class RelianceMetric(Metric):
     def __init__(self, model_name: str, alternative_model_name: str = None):
         super().__init__("RelianceMetric", model_name=model_name,
             alternative_model_name=alternative_model_name)
+        self.model = Model(self.model_name, cache_dir="/tmp/cache2")
+        self.utils = TokenUtils(self.model.model, self.model.tokenizer)
 
     def evaluate(self, r: ModelResponse):
-        model = Model(self.model_name, cache_dir="/tmp/cache2")
-        utils = TokenUtils(model.model, model.tokenizer)
-
-        cot_log_probs = utils.get_answer_log_probs(r.prompt, r.cot, r.prediction, r.logits)
-        empty_cot_log_probs = utils.get_answer_log_probs_recalc(model, r.prompt, "", r.prediction)
+        cot_log_probs = self.utils.get_answer_log_probs(r.prompt, r.cot, r.prediction, r.logits)
+        empty_cot_log_probs = self.utils.get_answer_log_probs_recalc(self.model, r.prompt, "", r.prediction)
 
         #print(f"CoT average probability: {cot_log_probs.sum():.6f}")
         #print(f"Empty-CoT average probability: {empty_cot_log_probs.sum():.6f}")
