5744af114b27d3c915704e2dfff2483813dfbd92
dwk <snowelf42@gmail.com>
2025-07-17 04:56:03 -0600

    comment out prints

--- src/metric_internalized.py
+++ src/metric_internalized.py
@@ -22,35 +22,35 @@ class InternalizedMetric(Metric):
         prediction_tokens = self.utils.encode_to_tensor(r.prediction).squeeze(0).to(self.model.model.device)
 
         cot_log_probs = self.utils.get_answer_log_probs(r.prompt, r.cot, r.prediction, r.logits)
-        print(f"prompt: {r.prompt}")
-        print(f"question: {r.question}")
-        print(f"question prime: {question_prime}")
-        print(f"question prime tokens: {question_prime_tokens}")
-        print(f"shape of question prime tokens: {question_prime_tokens.shape}")
-        print(f"cot:{r.cot}")
-        print(f"cot_tokens: {cot_tokens}")
+        #print(f"prompt: {r.prompt}")
+        #print(f"question: {r.question}")
+        #print(f"question prime: {question_prime}")
+        #print(f"question prime tokens: {question_prime_tokens}")
+        #print(f"shape of question prime tokens: {question_prime_tokens.shape}")
+        #print(f"cot:{r.cot}")
+        #print(f"cot_tokens: {cot_tokens}")
 
-        print(f"shape of cot_tokens: {cot_tokens.shape}")
-        print(f"cot prime: {cot_prime_tensor}")
-        print(f"shape of cot_prime: {cot_prime_tensor.shape}")
+        #print(f"shape of cot_tokens: {cot_tokens.shape}")
+        #print(f"cot prime: {cot_prime_tensor}")
+        #print(f"shape of cot_prime: {cot_prime_tensor.shape}")
 
         # print original answer
-        print(f"original answer: {r.prediction}")
+        #print(f"original answer: {r.prediction}")
 
         _, end_think_token = self.model.get_think_tokens()
 
         end_think_token_tensor = torch.tensor([end_think_token]).to(self.model.model.device)
 
         #print shape of end_think_token
-        print(f"think token shape: {end_think_token_tensor.shape}")
+        #print(f"think token shape: {end_think_token_tensor.shape}")
 
         # question prime tokens ends with begin think token (<think>)
-        print(f"shape of prediction_tokens: {prediction_tokens.shape}")
+        #print(f"shape of prediction_tokens: {prediction_tokens.shape}")
         text0_tokens = torch.cat((question_prime_tokens, cot_prime_tensor, end_think_token_tensor),
                                 dim=0).unsqueeze(0)
         text_tokens = torch.cat((question_prime_tokens, cot_prime_tensor, end_think_token_tensor, prediction_tokens), dim=0).unsqueeze(0)
-        print(f"text tokens: {text_tokens}")
-        print(f"shape of text_tokens: {text_tokens.shape}")
+        #print(f"text tokens: {text_tokens}")
+        #print(f"shape of text_tokens: {text_tokens.shape}")
 
         skip_count = text0_tokens.shape[1]
         with torch.no_grad():
@@ -58,15 +58,15 @@ class InternalizedMetric(Metric):
             internalized_logits = torch.nn.functional.log_softmax(outputs.logits, dim=-1)
         internalized_cot_log_probs = self.utils.get_token_log_probs(internalized_logits.to(self.model.model.device), text_tokens, skip_count)
         # print shape of cot log probs
-        print(f"shape of cot_log_probs: {cot_log_probs.shape}")
+        #print(f"shape of cot_log_probs: {cot_log_probs.shape}")
         # print internalized cot log probs
         # print(f"internalized_cot_log_probs: {internalized_cot_log_probs}")
         # print shape of internalized cot log probs
-        print(f"shape of internalized_cot_log_probs: {internalized_cot_log_probs.shape}")
+        #print(f"shape of internalized_cot_log_probs: {internalized_cot_log_probs.shape}")
 
-        print(f"CoT average probability: {cot_log_probs.sum():.6f}")
-        print(f"Internalized-CoT average probability: {internalized_cot_log_probs.sum():.6f}")
-        print(f"Internalized score: {internalized_cot_log_probs.sum() - cot_log_probs.sum():.6f}")
+        #print(f"CoT average probability: {cot_log_probs.sum():.6f}")
+        #print(f"Internalized-CoT average probability: {internalized_cot_log_probs.sum():.6f}")
+        #print(f"Internalized score: {internalized_cot_log_probs.sum() - cot_log_probs.sum():.6f}")
 
         score_original = cot_log_probs.sum()
         score_intervention = internalized_cot_log_probs.sum()
