96222594a390b768a83c9e528de4d897a5cdfa38
linhlt-it-ee <linhlt.it.ee@gmail.com>
2025-07-18 18:44:06 -0400

    bugfix mean vs sum

--- src/metric_transferability.py
+++ src/metric_transferability.py
@@ -23,20 +23,19 @@ class TransferabilityMetric(Metric):
         R1 = r1.cot
         A1 = r1.answer
         Q1 = r1.question
-        # print(r1)
 
-        log_probs1 = self.utils1.get_answer_log_probs_recalc(self.model1, r1.prompt, R1, A1)
+        log_probs1 = self.utils1.get_answer_log_probs_recalc(self.model1, r.prompt, r.cot, r.answer)
 
-        log_probs2 = self.utils2.get_answer_log_probs_recalc(self.model2, r1.prompt, R1, A1)
+        log_probs2 = self.utils2.get_answer_log_probs_recalc(self.model2, r.prompt, r.cot, r.answer)
 
-        log_probs3 = self.utils2.get_answer_log_probs_recalc(self.model2, r1.prompt, "", A1)
+        log_probs3 = self.utils2.get_answer_log_probs_recalc(self.model2, r.prompt, "", r.answer)
 
         # print(f"log_probs1: {log_probs1}\n\nlog_probs2: {log_probs2}")
         score1 = ((log_probs1.sum() - log_probs2.sum())/ log_probs1.sum())
         score2 = ((log_probs1.sum() - log_probs3.sum()) / log_probs1.sum())
-        output_path = Path(f"output/output_logprobs_{self.model1.model_name.split('/')[-1]}_{self.model2.model_name.split('/')[-1]}.jsonl")
+        output_path = Path(f"output/logprobs_{self.model1.model_name.split('/')[-1]}_{self.model2.model_name.split('/')[-1]}.jsonl")
         result={"score1":float(score1),"score2":float(score2),
-                "logprobsM1A1_sum": float(log_probs1.mean()), "logprobsM2_QR1A1_sum": float(log_probs2.mean()),"logprobsM2_QA1_sum": float(log_probs3.mean()),
+                "logprobsM1A1_sum": float(log_probs1.sum()), "logprobsM2_QR1A1_sum": float(log_probs2.sum()),"logprobsM2_QA1_sum": float(log_probs3.sum()),
                 "logprobsM1A1_mean":float(log_probs1.mean()), "logprobsM2_QR1A1_mean":float(log_probs2.mean()),"logprobsM2_QA1_mean":float(log_probs3.mean()),
                 "question":Q1,"answer":A1,"cot":R1}
         os.makedirs("output", exist_ok=True)
