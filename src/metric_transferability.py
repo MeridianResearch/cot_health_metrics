9bdbf8195c9f86867d782ebb6f668da1b74c0491
linhlt-it-ee <linhlt.it.ee@gmail.com>
2025-07-18 06:26:24 -0400

    update transfer

--- src/metric_transferability.py
+++ src/metric_transferability.py
@@ -2,12 +2,13 @@ import torch
 from metric import Metric
 from model import Model, ModelResponse
 from transformers import AutoTokenizer
-from common_utils import datasets_to_use,SupportedModel
 import json
 import pandas as pd
 from token_utils import TokenUtils
 from pathlib import Path
 import os
+# Current datetime
+from common_utils import get_datetime_str
 class TransferabilityMetric(Metric):
     def __init__(self, model_name: str, alternative_model_name: str = None):
         super().__init__("TransferabilityMetric", model_name=model_name,
@@ -21,6 +22,7 @@ class TransferabilityMetric(Metric):
         r1 = r
         R1 = r1.cot
         A1 = r1.answer
+        Q1 = r1.question
         # print(r1)
 
         log_probs1 = self.utils1.get_answer_log_probs_recalc(self.model1, r1.prompt, R1, A1)
@@ -32,9 +34,12 @@ class TransferabilityMetric(Metric):
         # print(f"log_probs1: {log_probs1}\n\nlog_probs2: {log_probs2}")
         score1 = ((log_probs1.sum() - log_probs2.sum())/ log_probs1.sum())
         score2 = ((log_probs1.sum() - log_probs3.sum()) / log_probs1.sum())
-        output_path = Path("output/output_logprobs.jsonl")
-        result={"score1":float(score1),"score2":float(score2), "logprobsM1A1":float(log_probs1.mean()), "logprobsM2_QR1A1":float(log_probs2.mean()),"logprobsM2_QA1":float(log_probs3.mean())}
+        output_path = Path(f"output/output_logprobs_{self.model1.model_name.split('/')[-1]}_{self.model2.model_name.split('/')[-1]}.jsonl")
+        result={"score1":float(score1),"score2":float(score2),
+                "logprobsM1A1_sum": float(log_probs1.mean()), "logprobsM2_QR1A1_sum": float(log_probs2.mean()),"logprobsM2_QA1_sum": float(log_probs3.mean()),
+                "logprobsM1A1_mean":float(log_probs1.mean()), "logprobsM2_QR1A1_mean":float(log_probs2.mean()),"logprobsM2_QA1_mean":float(log_probs3.mean()),
+                "question":Q1,"answer":A1,"cot":R1}
         os.makedirs("output", exist_ok=True)
         with output_path.open("a") as f:
             f.write(json.dumps(result) + "\n")
-        return (score1,log_probs1.mean(),log_probs2.mean())
+        return (score1,log_probs1.sum(),log_probs2.sum())
