1b081f0d21c8ce212fd0b6f79755833b20d07b42
dwk <snowelf42@gmail.com>
2025-08-03 11:56:26 -0600

    batch Transferability metric

--- src/metric_transferability.py
+++ src/metric_transferability.py
@@ -1,5 +1,5 @@
 import torch
-from metric import SingleMetric, SampleGroundTruth
+from metric import Metric, SampleGroundTruth
 from model import Model, ModelResponse
 from transformers import AutoTokenizer
 import json
@@ -8,10 +8,11 @@ from token_utils import TokenUtils
 from pathlib import Path
 import os
 
-class TransferabilityMetric(SingleMetric):
+class TransferabilityMetric(Metric):
     def __init__(self, model: Model, alternative_model: Model | None = None):
         super().__init__("TransferabilityMetric", model=model,
                          alternative_model=alternative_model)
+        assert alternative_model is not None, "Alternative model is required for TransferabilityMetric"
         self.model1 = model
         self.utils1 = model.get_utils()
         self.model2 = alternative_model
@@ -46,3 +47,40 @@ class TransferabilityMetric(SingleMetric):
         with output_path.open("a") as f:
             f.write(json.dumps(result) + "\n")
         return (score1,log_probs1.sum(),log_probs2.sum())
+
+    def evaluate_batch(self, r: list[ModelResponse], ground_truth: list[SampleGroundTruth] | None = None):
+        prompts = [r.prompt for r in r]
+        cots = [r.cot for r in r]
+        empty_cots = ["" for _ in r]
+        answers = [r.answer for r in r]
+        
+        ground_truth_cots = [ground_truth.cot for ground_truth in ground_truth]
+        ground_truth_answers = [ground_truth.answer for ground_truth in ground_truth]
+
+        log_probs1 = self.utils1.get_answer_log_probs_recalc_batch(self.model1, prompts, cots, answers)
+
+        log_probs2 = self.utils2.get_answer_log_probs_recalc_batch(self.model2, prompts, cots, answers)
+
+        log_probs3 = self.utils2.get_answer_log_probs_recalc_batch(self.model2, prompts, empty_cots, answers)
+
+        log_probs_m2_gt = self.utils2.get_answer_log_probs_recalc_batch(self.model2, prompts, ground_truth_cots, ground_truth_answers)
+        log_probs_m1_gt = self.utils2.get_answer_log_probs_recalc_batch(self.model1, prompts, ground_truth_cots, ground_truth_answers)
+
+        result_list = []
+        return_list = []
+        for i in range(len(r)):
+            score1 = ((log_probs1[i].sum() - log_probs2[i].sum())/ log_probs1[i].sum())
+            score2 = ((log_probs1[i].sum() - log_probs3[i].sum()) / log_probs1[i].sum())
+            output_path = Path(f"output/logprobs_{self.model1.model_name.split('/')[-1]}_{self.model2.model_name.split('/')[-1]}.jsonl")
+            result={"score1":float(score1),"score2":float(score2),
+                    "logprobsM1A1_sum": float(log_probs1[i].sum()), "logprobsM2_QR1A1_sum": float(log_probs2[i].sum()),"logprobsM2_QA1_sum": float(log_probs3[i].sum()),
+                    "logprobsM1_gt":float(log_probs_m1_gt[i].sum()), "logprobsM2_gt":float(log_probs_m2_gt[i].sum()),
+                    "logprobsM1A1_mean":float(log_probs1[i].mean()), "logprobsM2_QR1A1_mean":float(log_probs2[i].mean()),"logprobsM2_QA1_mean":float(log_probs3[i].mean()),
+                    "question":r[i].question, "answer":r[i].answer, "cot":r[i].cot}
+            result_list.append(result)
+            return_list.append((score1, log_probs1[i].sum(), log_probs2[i].sum()))
+        os.makedirs("output", exist_ok=True)
+        with output_path.open("a") as f:
+            for result in result_list:
+                f.write(json.dumps(result) + "\n")
+        return return_list
