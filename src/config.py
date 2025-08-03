e9182c750af3272423a7b7e526d1346d52c80e60
dwk <snowelf42@gmail.com>
2025-08-03 13:45:45 -0600

    dataset revamp for MMLU

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
+from random import sample
 from datasets import load_dataset, Dataset
+from typing import Callable
 
 CACHE_DIR_DEFAULT       = "hf_cache"
 LOG_DIRECTORY_DEFAULT   = "log"
@@ -62,34 +64,64 @@ class ModelConfig:
     def is_supported(model_name: str) -> bool:
         return model_name in ModelConfig.SUPPORTED_MODELS
 
+class DatasetAdapter:
+    def __init__(self, dataset_name: str, aliases: list[str],
+        load_section: str = "main", load_split: str = "train",
+        do_extract: Callable | None = lambda d: (d["question"], "", d["answer"])):
+
+        self.dataset_name = dataset_name
+        self.aliases = aliases
+        self.load_section = load_section
+        self.load_split = load_split
+        self.do_extract = do_extract
+
+    def name_matches(self, dataset_name: str) -> bool:
+        return dataset_name == self.dataset_name or dataset_name in self.aliases
+
+    def get(self, dataset_name: str) -> str:
+        if dataset_name == self.dataset_name:
+            return self.dataset_name
+        if dataset_name in self.aliases:
+            return self.dataset_name
+        raise ValueError(f"Dataset {self.dataset_name} passed invalid alias {dataset_name}")
+
+    def extract_pieces(self, sample_data) -> tuple[str, str, str]:
+        return self.do_extract(sample_data)
+
+    def load(self, dataset_name: str, max_samples: int | None = None) -> Dataset:
+        split = self.load_split
+        if max_samples is not None:
+            split = split + f"[:{max_samples}]"
+        print(f"Loading dataset {dataset_name} with spplit {split}")
+        print(f"Dataset name: {self.get(dataset_name)}")
+        print(f"Stored Dataset name: {self.dataset_name}")
+        return load_dataset(self.get(dataset_name), self.load_section, split=split)
+
 class DatasetConfig:
-    HF_DATASET_NAMES = {
-        "Alpaca": "vicgalle/alpaca-gpt4",
-        "GSM8K": "gsm8k",
-        "MMLU": "openai/openai_mmlu_benchmark",
-    }
-
-    HF_DATASET_COT_ANSWER_SPLIT = {
-        "GSM8K": lambda d: d["answer"].split("####"),
-    }
-
-    @staticmethod
-    def get(dataset_name: str) -> str:
-        if dataset_name not in DatasetConfig.HF_DATASET_NAMES:
-            return dataset_name
-        return DatasetConfig.HF_DATASET_NAMES[dataset_name]
-    
-    @staticmethod
-    def do_cot_answer_split(dataset_name: str, sample_data) -> tuple[str, str]:
-        if dataset_name not in DatasetConfig.HF_DATASET_COT_ANSWER_SPLIT:
-            return ('', '')
-        func = DatasetConfig.HF_DATASET_COT_ANSWER_SPLIT[dataset_name]
-        try:
-            return func(sample_data)
-        except Exception as e:
-            print(f"Error splitting cot and answer for sample \"{sample_data}\" in dataset {dataset_name}: {e}")
-            return ('', '')
+    HF_DATASET_LIST = [
+        DatasetAdapter("vicgalle/alpaca-gpt4", ["alpaca", "alpaca-gpt4"],
+            do_extract=lambda d: (d["question"], "", d["answer"])),
+        DatasetAdapter("gsm8k", ["GSM8K"],
+            do_extract=lambda d: (d["question"], d["answer"].split("####")[0], d["answer"].split("####")[1])),
+        DatasetAdapter("cais/mmlu", ["MMLU", "mmlu"], load_section="astronomy", load_split="dev",
+            do_extract=lambda d: (d["question"] + "\nChoices: "
+                + [f"{chr(ord('A')+i)}: {d['choices'][i]}\n" for i in range(len(d["choices"]))],
+                "", d["answer"])),
+    ]
+    HF_DATASET_NAMES = {adapter.dataset_name: adapter for adapter in HF_DATASET_LIST}
+    HF_DATASET_ALIASES = {alias: adapter for adapter in HF_DATASET_LIST for alias in adapter.aliases}
 
     @staticmethod
-    def load(dataset_name: str, **kwargs) -> Dataset:
-        return load_dataset(dataset_name, "main", **kwargs)
+    def get(dataset_name: str) -> DatasetAdapter:
+        print(f"HF_DATASET_NAMES: {DatasetConfig.HF_DATASET_NAMES}")
+        print(f"HF_DATASET_ALIASES: {DatasetConfig.HF_DATASET_ALIASES}")
+        if dataset_name in DatasetConfig.HF_DATASET_NAMES:
+            return DatasetConfig.HF_DATASET_NAMES[dataset_name]
+        if dataset_name in DatasetConfig.HF_DATASET_ALIASES:
+            return DatasetConfig.HF_DATASET_ALIASES[dataset_name]
+        return DatasetAdapter(dataset_name, [])
+
+    @staticmethod
+    def load(dataset_name: str, max_samples: int | None = None) -> Dataset:
+        dataset = DatasetConfig.get(dataset_name)
+        return dataset.load(dataset_name, max_samples)
