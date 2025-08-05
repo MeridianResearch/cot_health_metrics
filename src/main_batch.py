0c1c4bdf2d2ff3465a7776785ed9a290fe046e73
IdaCy <ida.caspary@gmail.com>
2025-08-05 09:12:57 -0600

    integrated new metric in parcel

--- src/main_batch.py
+++ src/main_batch.py
@@ -4,6 +4,7 @@ python src/main_batch.py --model=Qwen/Qwen3-0.6B --metric=Internalized --data-hf
 python src/main_batch.py --model=Qwen/Qwen3-0.6B --model2=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B --metric=Transferability --data-hf=GSM8K --max-samples=2
 python src/main_batch.py --model=Qwen/Qwen3-0.6B --metric=Paraphrasability --data-hf=GSM8K --max-samples=2
 python src/main_batch.py --model=Qwen/Qwen3-0.6B --metric=ParaphrasabilitySimple --data-hf=GSM8K --max-samples=2
+python src/main_batch.py --model=Qwen/Qwen3-0.6B --metric=PromptParaphrasability --data-hf=GSM8K --max-samples=2
 """
 
 import argparse
