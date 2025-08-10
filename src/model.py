74b88e72a6e5f6d85cdf90e295817bb36d31bee1
dwk <snowelf42@gmail.com>
2025-08-10 15:02:17 -0600

    add ModelPromptBuilder, better names

--- src/model.py
+++ src/model.py
@@ -82,6 +82,78 @@ class ModelComponentFactory:
             return ModelPromptBuilder(self.model_name, invokes_cot=invokes_cot)
         return self.construct_prompt_builder(self.model_name, invokes_cot=invokes_cot)
 
+class ModelPromptBuilder:
+    def __init__(self, model_name: str, invokes_cot: bool = True):
+        self.model_name = model_name
+        self.invokes_cot = invokes_cot
+        self.question = None
+
+        # default to making a new assistant role section
+        self.continue_final_message = False
+        self.add_generation_prompt = True
+        self.history = []
+
+    def get_model_custom_instruction(self):
+        please_write_answer = "Please write the string \"Answer: \" before the final answer."
+
+        if self.model_name == "google/gemma-2-2b-it":
+            return please_write_answer
+        if self.model_name == "meta-llama/Meta-Llama-3-8B-Instruct" or self.model_name == "meta-llama/Llama-2-7b-chat-hf":
+            return please_write_answer
+
+        return None
+
+    def add_system_instruction(self, system_instruction: str):
+        self.add_to_history("system", system_instruction)
+
+    def add_user_message(self, question: str, custom_instruction: str = "Let's think step by step."):
+        self.question = question
+        model_custom_instruction = self.get_model_custom_instruction()
+        if model_custom_instruction is not None:
+            custom_instruction = custom_instruction + " " + model_custom_instruction
+        self.add_to_history("user", f"Question: {question}\n{custom_instruction}")
+
+    def add_to_history(self, role: str, content: str):
+        assert self.continue_final_message == False
+
+        self.history.append({
+            "role": role,
+            "content": content
+        })
+
+    def add_partial_to_history(self, role: str, content: str):
+        assert self.continue_final_message == False
+
+        self.history.append({
+            "role": role,
+            "content": content
+        })
+        self.continue_final_message = True
+        self.add_generation_prompt = False
+
+    def add_think_token(self):
+        model_config = ModelConfig.get(self.model_name)
+        if "begin_think" in model_config:
+            if (self.model_name == "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"):
+                self.add_partial_to_history("assistant", "<think>")
+        elif "fuzzy_end_think_list" in model_config:
+            pass
+        else:
+            print(f"ERROR: model {self.model_name} missing CoT separator config")
+            exit(1)
+
+def make_prompt(self, tokenizer):
+        if self.invokes_cot:
+            self.add_think_token()
+        return self._apply_chat_template(tokenizer)
+
+    def _apply_chat_template(self, tokenizer):
+        prompt = tokenizer.apply_chat_template(self.history,
+                                               tokenize=False,
+                                               add_generation_prompt=self.add_generation_prompt,
+                                               continue_final_message=self.continue_final_message)
+        return prompt
+
 class CoTModel(Model):
     def __init__(self, model_name: str,
         component_factory: ModelComponentFactory = None,
@@ -144,13 +216,13 @@ class CoTModel(Model):
         responses = self.generate_cot_response_full_batch(question_ids, questions, max_new_tokens)
         return [response.basic_pair for response in responses]
 
-    def make_prompt(self, question_id, question, custom_instruction=None):
-        prompt_builder = self.component_factory.make_prompt_builder(invokes_cot=True)
+    def make_prompt(self, question_id, question, custom_instruction="Let's think step by step."):
+        prompt_builder = ModelPromptBuilder(self.model_name, invokes_cot=True)
         prompt_builder.add_user_message(question, custom_instruction)
         return prompt_builder.make_prompt(self.tokenizer)
 
     def make_prompt_no_cot(self, question_id, question):
-        prompt_builder = self.component_factory.make_prompt_builder(invokes_cot=False)
+        prompt_builder = ModelPromptBuilder(self.model_name, invokes_cot=False)
         prompt_builder.add_user_message(question)
         return prompt_builder.make_prompt(self.tokenizer)
 
