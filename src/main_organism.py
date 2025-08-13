41ca6b0130e3b408df7077eda7c691f0d085e1e1
dwk <snowelf42@gmail.com>
2025-08-13 12:27:41 -0600

    new organism stuff

---
+++ src/main_organism.py
@@ -0,0 +1,160 @@
+import argparse
+import os
+import sys
+import json
+from typing import List, Iterator
+from datetime import datetime
+from config import DatasetConfig
+from data_loader import load_prompts
+from datasets import Dataset
+
+from model import CoTModel
+from model_factory import ModelComponentFactory
+from model_prompts import CustomInstructionPromptBuilder
+from config import CACHE_DIR_DEFAULT, LOG_DIRECTORY_DEFAULT, LOG_EVERY_DEFAULT
+from all_organisms import OrganismRegistry
+
+# Current datetime
+now = datetime.now()
+
+# Format as string
+def _get_datetime_str():
+    datetime_str = now.strftime("%Y-%m-%d_%H:%M:%S")
+    print(datetime_str)
+    return datetime_str
+
+def _get_sample_question(sample: dict) -> str:
+    question = sample["instruction"].strip()
+    if sample.get("input"):
+        question += " " + sample["input"].strip()
+    return question
+
+def _iterate_dataset(dataset_name: str, dataset: Dataset) -> Iterator[tuple[int, str, str, str]]:
+    adapter = DatasetConfig.get(dataset_name)
+    for i, d in enumerate(dataset):
+        pieces = adapter.extract_pieces(d)
+        yield (i, *pieces)
+
+def _iterate_local_dataset(prompts: List[dict]) -> Iterator[tuple[int, str, str, str]]:
+    for p in prompts:
+        yield (p['prompt_id'], _get_sample_question(p), '', '')
+
+def print_output(id, question, prompt, cot, answer, f, f_json, args, organism):
+    print(f"{id}\t{question}")
+
+    f.write(f"{id}\t{question}")
+    f.flush()
+
+    output = {
+        "prompt_id": id,
+        "organism": organism.get_name(),
+        "model": organism.get_default_model_name(),
+        "question": question,
+        "cot": cot,
+        "answer": answer
+    }
+    f_json.write(json.dumps(output) + "\n")
+    f_json.flush()
+
+def handle_datapoints(datapoints, args, model, f, f_json, organism):
+    log_counter = 0
+    for i, (id, question, ground_truth_cot, ground_truth_answer) in enumerate(datapoints):
+        if i < args.skip_samples:
+            continue
+
+        try:
+            r = model.generate_cot_response_full(id, question)
+            r.prompt_id = id
+        except RuntimeError as err:
+            print(f"Sample id={id} - generation error ({err})")
+            continue
+
+        if log_counter % args.log_every == 0:
+            #print(f"Sample id={id} - {result.score:.4f}")
+            pass
+        log_counter += 1
+
+        print_output(id, question, r.prompt, r.cot, r.answer, f, f_json, args, organism)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--organism", required=True)
+    parser.add_argument("--model", default=None)
+    parser.add_argument("--model2", default=None)
+    parser.add_argument("--data-path", default=None)
+    parser.add_argument("--data-hf", default=None)
+    parser.add_argument("--skip-samples", type=int, default=0)
+    parser.add_argument("--max-samples", type=int, default=None)
+    parser.add_argument("--cache-dir", default=CACHE_DIR_DEFAULT)
+    parser.add_argument("--log-dir", default=LOG_DIRECTORY_DEFAULT)
+    parser.add_argument("--log-file", default=None)
+    parser.add_argument("--log-every", type=int, default=LOG_EVERY_DEFAULT)
+    parser.add_argument("--log-verbose", type=bool, default=True)
+
+    parser.add_argument("--filler", type=str, default="think")  # Internalized
+    parser.add_argument("--filler-in-prompt", type=bool, default=False)  # Internalized
+    parser.add_argument("--filler-in-cot", type=bool, default=False)  # Internalized
+
+    organism_registry = OrganismRegistry()
+
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        print("\nValid organisms (with default model):")
+        for (name, organism) in organism_registry.get_all():
+            print("    %-30s %s" % (name, organism.get_default_model_name()))
+        sys.exit(1)
+
+    # Load dataset
+    dataset_name = ''
+    if args.data_hf:
+        dataset_name = args.data_hf
+        if args.max_samples:
+            dataset = DatasetConfig.load(dataset_name, max_samples=args.max_samples)
+        else:
+            dataset = DatasetConfig.load(dataset_name)
+
+        datapoints = _iterate_dataset(dataset_name, dataset)
+    elif args.data_path:
+        dataset_name = os.path.basename(args.data_path)
+        prompts: List[dict] = load_prompts(args.data_path, args.max_samples)
+
+        datapoints = _iterate_local_dataset(prompts)
+    else:
+        raise ValueError("Either --data-hf or --data-path must be provided")
+
+    # Make cache dir
+    os.makedirs(args.cache_dir, exist_ok=True)
+
+    # Get organism
+    organism = organism_registry.get(args.organism)
+    if organism is None:
+        raise ValueError(f"Organism {args.organism} not found")
+
+    # Get model name
+    model_name = args.model
+    if model_name is None or model_name == "":
+        model_name = organism.get_default_model_name()
+
+    # Load model
+    component_factory = organism.get_component_factory(model_name)
+    model = CoTModel(model_name, component_factory=component_factory, cache_dir=args.cache_dir)
+
+    if args.log_file is None:
+        base = args.log_dir + "/" + organism.get_name() + "_" + model_name + "_" + dataset_name + "_" + _get_datetime_str()
+        log_file = base + ".log"
+        json_log_file = base + ".jsonl"
+        config_log_file = base + ".config.json"
+    else:
+        log_file = args.log_file
+        json_log_file = log_file + ".jsonl"
+        config_log_file = log_file + ".config.jsonl"
+
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    with open(log_file, 'a') as f:
+        with open(json_log_file, 'a') as f_json:
+            handle_datapoints(datapoints, args, model, f, f_json, organism)
+
+if __name__ == "__main__":
+    main()
--- src/organism.py
+++ src/organism.py
@@ -1,108 +1,34 @@
-import torch, copy
-from transformers.cache_utils import StaticCache
-from model import CoTModel
+from model_prompts import CustomInstructionPromptBuilder
+from model_factory import ModelComponentFactory
 
+class Organism:
+    def __init__(self, name: str, default_model_name: str):
+        self.name = name
+        self.default_model_name = default_model_name
 
-class ModelOrganism(CoTModel):
-    pass
+    def get_name(self):
+        return self.name
 
+    def get_default_model_name(self):
+        return self.default_model_name
 
+    def get_component_factory(self, model_name: str | None = None) -> ModelComponentFactory | None:
+        return None
 
-class SystemPromptModelOrganism(ModelOrganism):
-    
-    system_prompt: str
-    formatted_system_prompt: str
-    system_prompt_cache: StaticCache
+class SystemPromptOrganism(Organism):
+    def __init__(self, name: str, default_model_name: str, custom_instruction: str = "", custom_assistant_prefix: str = ""):
+        super().__init__(name, default_model_name)
+        self.custom_instruction = custom_instruction
+        self.custom_assistant_prefix = custom_assistant_prefix
 
-    def __init__(self, system_prompt, model_name, cache_dir="/tmp/cache"):
-        super().__init__(model_name, cache_dir)
+    def _construct_prompt_builder(self, model_name: str, invokes_cot: bool):
+        return CustomInstructionPromptBuilder(
+            model_name,
+            custom_instruction=self.custom_instruction,
+            custom_assistant_prefix=self.custom_assistant_prefix)
 
-        self.system_prompt = system_prompt
-
-        sp_history = [
-            {"role": "system", "content": self.system_prompt},
-        ]
-
-        self.formatted_system_prompt = self.tokenizer.apply_chat_template(
-            sp_history,
-            tokenize = False,
-            add_generation_prompt= False,
-            continue_final_message = False,
-            skip_special_tokens = False
-        )
-
-        import pdb; pdb.set_trace(header = 'check self.formatted_system_prompt is legit here!')
-
-        inputs = self.tokenizer(
-            self.formatted_system_prompt, 
-            return_tensors="pt", 
-            padding=False, 
-            truncation=False,
-            add_special_tokens=False
-        ).to(self.device)
-        
-        with torch.no_grad():
-            outputs = self.model(
-                input_ids=inputs.input_ids,
-                attention_mask=inputs.attention_mask,
-                use_cache=True,
-                return_dict=True
-            )
-
-        import pdb; pdb.set_trace(header = 'check inputs.attention_mask and outputs.attention_mask are both legit here!')
-
-        self.system_prompt_cache = outputs.past_key_values
-    
-    @torch.no_grad()    # FIXME: can we add this to other places in CoTModel
-    def do_generate(self, question_id, prompt, max_new_tokens=4096):
-        
-        # FIXME: address next line
-        import pdb; pdb.set_trace(header = 'make sure there is no existing system prompt here -- might have to edit CoTModel.make_prompt for this, I have not checked')
-
-        prompt = self.formatted_system_prompt + prompt
-        past_key_values_copy = copy.deepcopy(self.system_prompt_cache)  # Cache is written to inplace internally
-        
-        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
-        output = self.model.generate(
-            **inputs,
-            max_new_tokens=max_new_tokens,
-
-            # FIXME: can we add this to an experiment config?
-            do_sample=True,
-            temperature=0.6,
-            top_k=20,
-            min_p=0.0,
-            top_p=0.95,
-
-            eos_token_id=self.tokenizer.eos_token_id,
-            pad_token_id=self.tokenizer.eos_token_id,
-            output_scores=True,
-            return_dict_in_generate=True,
-            past_key_values=past_key_values_copy,
-        )
-
-        # FIXME: for the sake of do_split and other downstream functionality, you should check whether the system prompt is included in these outputs!
-        # It would be good to remove it here, so that downstream functionality can be cleaner
-        # Alternatively, we might have to remove the system prompt from the decoded sequence in generate_cot_response_full ??
-        import pdb; pdb.set_trace(header = 'make sure outputs look legit here!')
-
-        return output
-
-    def get_log_probs(self, sequences: torch.Tensor):
-
-        bsz = sequences.shape[0]
-        rep_func = lambda x: x.expand(bsz, *x.shape[1:])
-
-        # Duplicate cache for all items in the inputted sequence
-        past_key_values = copy.deepcopy(self.system_prompt_cache)
-        past_key_values.key_cache = list(map(rep_func, past_key_values.key_cache))
-        past_key_values.value_cache = list(map(rep_func, past_key_values.value_cache))
-
-        with torch.no_grad():
-            outputs = self.model(input_ids=sequences, past_key_values = past_key_values)
-            log_probs = torch.nn.functional.log_softmax(outputs.logits, dim=-1)
-        
-        # FIXME: should we select by true token here?? this should be shaped [batch, sequence length - 1]
-
-        return log_probs
-
+    def get_component_factory(self, model_name: str | None = None) -> ModelComponentFactory | None:
+        if model_name is None:
+            model_name = self.default_model_name
+        return ModelComponentFactory(model_name,
+            construct_prompt_builder=self._construct_prompt_builder)
