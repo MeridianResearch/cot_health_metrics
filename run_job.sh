bc92315eeb344a156101cb077626135295f2ac69
Manqing Liu <manqingliu@g.harvard.edu>
2025-08-17 20:01:22 -0400

    add other models to test capabilities

--- run_job.sh
+++ run_job.sh
@@ -3,11 +3,13 @@
 #python src/main_batch.py --model=openai/gpt-oss-20b --metric=Internalized --data-hf=GSM8K --log-dir=data/logprobs/json --max-samples=401 --filler=lorem_ipsum --filler-in-prompt
 #python src/plot_metric_logprobs.py --metric-name internalized --input-path data/logprobs/json/openai/gpt-oss-20b_GSM8K_2025-08-11_21:32:16_Internalized_filler_lorem_ipsum_prompt_.jsonl --out-dir data/plots/internalized --filler=lorem_ipsum
 
-python src/main_organism.py \
-  --model deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
-  --icl-examples-file data/icl_examples/icl_think_token_default.json \
-  --data-hf gsm8k \
-  --organism icl-custom \
-  --icl-filler think \
-  --max-samples 3
+#python src/main_organism.py \
+#  --model Qwen/Qwen3-8B \
+#  --icl-examples-file data/icl_examples/icl_lorem_ipsum_default.json \
+#  --data-hf gsm8k \
+#  --organism icl-custom \
+#  --icl-filler lorem_ipsum \
+#  --max-samples 50
 
+
+python src/analyze_accuracy_gsm8k.py log/icl-lorem_ipsum_Qwen/Qwen3-8B_gsm8k_lorem_ipsum_2025-08-17_22:15:47.jsonl
