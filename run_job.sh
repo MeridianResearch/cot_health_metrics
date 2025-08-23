13b47e9f5c43a260c4aae05869cc9163a728e98b
Manqing Liu <manqingliu@g.harvard.edu>
2025-08-23 11:45:01 -0400

    test icl on qwen 0.6B

--- run_job.sh
+++ run_job.sh
@@ -11,13 +11,20 @@
 #  --icl-filler lorem_ipsum \
 #  --max-samples 1
 
-python src/main_organism.py
-    --model Qwen/Qwen3-8B
-    --data-hf gsm8k
-    --organism standard-cot
-    --max-samples 1
-
+#python src/main_organism.py \
+#  --model Qwen/Qwen3-0.6B \
+#  --icl-examples-file data/icl_examples/icl_lorum_ipsum_default_5_fewshot.json \
+#  --data-hf gsm8k \
+#  --organism icl-think \
+#  --max-samples 100
 
 #python src/analyze_accuracy_gsm8k.py log/icl-think_token_Qwen/Qwen3-8B_gsm8k_think_2025-08-18_22:02:58.jsonl
 
 #python src/analyze_accuracy_gsm8k.py log/icl-lorem_ipsum_openai/gpt-oss-20b_gsm8k_lorem_ipsum_2025-08-18_21:23:23.jsonl
+
+
+mkdir -p log/accuracy_gsm8k
+LOGFILE="log/icl-think_Qwen/Qwen3-0.6B_gsm8k_2025-08-22_23:00:49.jsonl"
+BASENAME="$(basename "$LOGFILE" .jsonl)"
+python src/analyze_accuracy_gsm8k.py "$LOGFILE" | tee "log/accuracy_gsm8k/${BASENAME}.analysis.txt"
+
