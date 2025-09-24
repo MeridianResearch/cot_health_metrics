840ccbc7bd6964cd13bc992006c075c32dbd87f5
Thai.Linh Le <let@login-2.server.mila.quebec>
2025-09-24 16:36:20 -0400

    upd

--- pipeline1.sh
+++ pipeline1.sh
@@ -3,6 +3,7 @@
 stage=$1
 dataset=$2
 max_samples=$3
+paraphrase_mode=google
 
 if [ -z "$stage" ]; then
     echo Usage: $0 stage [dataset [max_samples]]
@@ -19,6 +20,7 @@ fi
 
 if [ -z "$GOOGLE_API_KEY" ]; then
     echo "WARNING: GOOGLE_API_KEY not set, will use simple paraphrasing"
+    paraphrase_mode=local
 fi
 
 cmd="python3"
@@ -27,10 +29,13 @@ if [ "$stage" -eq 0 ]; then
 fi
 
 #Qwen/Qwen3-0.6B Qwen/Qwen3-1.7B Qwen/Qwen3-4B Qwen/Qwen3-8B Qwen/Qwen3-8B Qwen/Qwen3-14B deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B google/gemma-2-2b-it microsoft/phi-2 meta-llama/Meta-Llama-3-8B-Instruct meta-llama/Llama-2-7b-chat-hf mistralai/Mistral-7B-Instruct-v0.3
-model=Qwen/Qwen3-8B
-model2=Qwen/Qwen3-4B
+#model=google/gemma-2-9b-it
+model=openai/gpt-oss-20b
+model2=Qwen/Qwen3-8B
 cache_dir=cache
-log_dir=data/pipeline
+log_dir=data/pipeline/para-$paraphrase_mode/$(basename $model)
+
+cmd="./submit_any_arbitrary.sh cot-$(basename $model) python3"
 
 mkdir -p $log_dir
 echo "Logging to $log_dir"
@@ -54,7 +59,7 @@ if [ $stage -le 1 ]; then
     done
 fi
 
-if [ $stage -le 10 ]; then
+if [ $stage -eq 10 ]; then
     echo === Stage 10, evaluation metric
     for metric in Reliance Internalized_filler_think_cot Transferability Paraphrasability; do
         echo Running $metric
