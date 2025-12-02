2ec08db722b0589b6ff976e881d3d23bcc8b1e6a
dwk <snowelf42@gmail.com>
2025-12-01 23:59:20 -0700

    testing script locally

--- test-metric.sh
+++ test-metric.sh
@@ -7,11 +7,22 @@ for lorapath in $(cat list.1); do
     lora=$(basename $(dirname $lorapath))
     echo $lora
 
+    model=$(jq -r .model $lorapath/../training_args.json)
+
     #for metric in Reliance Internalized Transferability Paraphrasability; do
     #for metric in Reliance Internalized Paraphrasability; do
     for dataset in binary_alternation calendar_arithmetic largest_island spell_backward string_manipulation; do
         #for metric in Reliance Internalized Paraphrasability; do
         for metric in Necessity Substantivity Paraphrasability; do
+            echo --- run $model $lorapath $metric $dataset
+            echo python src/main_batch.py \
+                --cache-dir ../models \
+                --model $model \
+                --adapter-path $lorapath \
+                --metric $metric \
+                --max-samples 2100 \
+                --skip-samples 2000 \
+                --data-hf $dataset
             python src/main_batch.py \
                 --cache-dir ../models \
                 --model $model \
